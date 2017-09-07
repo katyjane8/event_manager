5c1b0af5c97c96e57ea025a3e35b19a292b30e45
katyjane8 <katy.jane8@gmail.com>
2017-09-07 13:12:18 -0600

    working with the csv file

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,11 @@
 puts "EventManager Initialized!"
+
+#read the file contents
+# contents = File.read "event_attendees.csv"
+# puts contents
+
+#read the file line by line
+lines = File.readlines "event_attendees.csv"
+lines.each do |line|
+  puts line
+end

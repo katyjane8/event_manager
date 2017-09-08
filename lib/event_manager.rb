a60153f656d7ea9831b741c3e53d7863f1cd0f64
katyjane8 <katy.jane8@gmail.com>
2017-09-07 21:03:10 -0600

    adding steps to index

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,6 +6,15 @@ puts "EventManager Initialized!"
 
 #read the file line by line
 lines = File.readlines "event_attendees.csv"
-lines.each do |line|
-  puts line
+#track the index of the current line
+# row_index = 0
+lines.each_with_index do |line,index|
+  # ^^ This is a such a common operation that Array defines Array#each_with_index.
+  #skipping the header row
+  # row_index = row_index + 1
+  next if index == 0
+  #skipping the header row (cont.)
+  columns = line.split(",")
+  name = columns[2]
+  puts name
 end
